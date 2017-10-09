3d939bb025f190fab2f537844978422d1d9a2641
Xong Xiong <xxiong3153@gmail.com>
2017-10-09 14:37:07 -0500

    working on swapping colors

--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,24 @@
 $(document).ready(main);
 
 function main(){
+  $('#genBut').on('click', generate);
+  $('.colorLine').on('click', '#changeColor', toggleColor);
+}
 
+var backColor = 0;
+var butClicked = 0;
+
+function generate(){
+  butClicked++;
+    $('.container').append('<div class="colorLine"><p>' + butClicked +
+    '</p><button id="changeColor">Swap</button><button id="deleteLine">Delete</button></div>');
+    backColor = $('.colorLine').css('background-color');
+    console.log(backColor);
+    return backColor;
+}
+
+
+
+function toggleColor(){
+  alert('yelll!');
 }
